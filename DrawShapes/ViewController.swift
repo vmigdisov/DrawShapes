ce5464ac4acd092ae5832ef4013f9e9b1e35a1af
Мигдисов Всеволод <vmigdisov@it-one.ru>
2022-06-11 17:58:25 +0300

    first commit

--- DrawShapes/ViewController.swift
+++ DrawShapes/ViewController.swift
@@ -8,10 +8,24 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .white
+        let triangleView = UIView()
+        triangleView.center = view.center
+        triangleView.frame.size = CGSize(width: 16, height: 8)
+        triangleView.translatesAutoresizingMaskIntoConstraints = false
+        let path = UIBezierPath()
+        path.move(to: CGPoint(x: 0, y: 8))
+        path.addLine(to: CGPoint(x: 8, y: 0))
+        path.addLine(to: CGPoint(x: 16, y: 8))
+        path.close()
+        let shapeLayer = CAShapeLayer()
+        shapeLayer.path = path.cgPath
+        shapeLayer.fillColor = UIColor.gray.cgColor
+        triangleView.layer.addSublayer(shapeLayer)
+        view.addSubview(triangleView)
     }
 
 
