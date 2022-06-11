962b44b2d4a41ed96b3a6275a1c7d318daffa3ad
Мигдисов Всеволод <vmigdisov@it-one.ru>
2022-06-11 17:52:57 +0300

    Initial Commit

---
+++ DrawShapes/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DrawShapes
+//
+//  Created by Vsevolod Migdisov on 11.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
